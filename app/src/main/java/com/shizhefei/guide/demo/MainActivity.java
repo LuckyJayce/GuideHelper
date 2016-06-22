54759c7df980c52def5fa538b17243be7b3556a4
LuckyJayce <794629068@qq.com>
2016-06-23 00:31:39 +0800

    修改为类库引用 去除style，现在只需要引用GuideHelper一个类即可

--- app/src/main/java/com/shizhefei/guide/demo/MainActivity.java
+++ app/src/main/java/com/shizhefei/guide/demo/MainActivity.java
@@ -1,10 +1,11 @@
-package com.shizhefei.guide;
+package com.shizhefei.guide.demo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
 import android.view.View;
 
+import com.shizhefei.guide.GuideHelper;
 import com.shizhefei.guide.GuideHelper.TipData;
 import com.shizhefei.util.DisplayUtils;
 
@@ -19,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
         iconView = findViewById(R.id.icon);
